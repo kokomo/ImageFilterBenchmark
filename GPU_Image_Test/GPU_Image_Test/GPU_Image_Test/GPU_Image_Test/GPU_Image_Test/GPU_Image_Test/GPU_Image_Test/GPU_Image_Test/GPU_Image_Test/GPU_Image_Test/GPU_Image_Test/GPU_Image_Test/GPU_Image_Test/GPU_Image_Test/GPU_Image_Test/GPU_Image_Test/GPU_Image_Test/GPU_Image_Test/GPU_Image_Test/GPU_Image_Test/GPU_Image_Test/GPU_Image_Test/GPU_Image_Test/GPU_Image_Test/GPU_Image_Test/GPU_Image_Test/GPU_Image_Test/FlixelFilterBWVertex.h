6cb49fd69fac4fb92035d07e8a4345884ebe30ac
Colin McDonald <colinmcdonald@gmail.com>
2012-07-02 23:12:48 -0400

    Trying to learn how to ignore user setting files in Xcode which are included by default..

---
+++ GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/GPU_Image_Test/FlixelFilterBWVertex.h
@@ -0,0 +1,20 @@
+//
+//  FlixelFilterBWVertex.h
+//  GPU_Image_Test
+//
+//  Created by Colin McDonald on 12-06-29.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GPUImageFilter.h"
+
+/*
+ Exported using GLSL Studio: 2012-06-28 10:44 AM
+ Name: Black and white vertex
+ Author: flixel
+ */
+
+@interface FlixelFilterBWVertex : GPUImageFilter
+
+@end
